7141aae7cbb929de6251a2824087dba5de9ac7ad
Guilherme <guilherme.martins@alkasoft.com.br>
2024-08-21 15:02:15 -0300

    supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wcbpdqysjkunetgnyads.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjYnBkcXlzamt1bmV0Z255YWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyNjA0NzAsImV4cCI6MjAzOTgzNjQ3MH0.2c2A08KUOLNBtlXkfHwKJ9jIdu3Ueti8hoqvDjnMZEU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
