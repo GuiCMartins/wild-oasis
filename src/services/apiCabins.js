20fc66b514f833d8a691f7e96c496589a5e74251
Guilherme <guilherme.martins@alkasoft.com.br>
2024-08-23 10:36:44 -0300

    Update settings

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -36,6 +36,8 @@ export async function createEditCabin(newCabin, id) {
     throw new Error("Cabins could not be created");
   }
 
+  if (hasImageUrl) return data;
+
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
