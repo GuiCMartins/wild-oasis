f8127891c71512af7adafb63150c9a73128c2b00
Guilherme <guilherme.martins@alkasoft.com.br>
2024-09-29 15:11:06 -0300

    pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -15,14 +15,12 @@ export function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  const {
-    isLoading,
-    data: bookings,
-    error,
-  } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings, error };
+  return { isLoading, bookings: data?.data, error, count: data?.count };
 }
