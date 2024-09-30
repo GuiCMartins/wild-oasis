38b0f11d3f31d508316aac2a2024c07ca3660452
Guilherme <guilherme.martins@alkasoft.com.br>
2024-09-30 20:54:29 -0300

    Checkout

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isLoadingCheckout } = useCheckout();
   const navigate = useNavigate();
 
   const moveBack = useMoveBack();
@@ -51,6 +53,13 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${id}`)}>Check in</Button>
         )}
+
+        {status === "checked-in" && (
+          <Button onClick={() => checkout(id)} disabled={isLoadingCheckout}>
+            Check out
+          </Button>
+        )}
+
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
