8e15ae9cd2dcac98a0ccf67553e222d708fe1718
Guilherme <guilherme.martins@alkasoft.com.br>
2024-10-03 20:40:39 -0300

    logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,8 +10,8 @@ export default function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
     onError: () => {
       toast.error("Provided email or password are incorrect");
